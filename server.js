77c29bc5c66466c932f251f72d51fce0c621be13
Oliver Speir <oliverspeir9@gmail.com>
2022-11-17 02:42:52 +0000

    lab08

--- server.js
+++ server.js
@@ -10,27 +10,50 @@ let data = require('./data/weather.json');
 require('dotenv').config();
 
 const cors = require('cors');
-
+const axios = require('axios');
 const app = express();
 
 app.use(cors());
 
 const PORT = process.env.PORT
 
-app.get('/weather', (request, response, next) => {
+app.get('/weather', async (request, response, next) => {
     try {
         // let dirtyReq = request.query; 
         // let Req = Object.keys(dirtyReq)[0];
         // let cleanReq = Req.slice(0,Req.indexOf(','));
-        let city = request.query.cityName;
-        let selectedCity = data.find(x => x.city_name === city);
-        const forecastArray = selectedCity.data.map(day => new Forecast(day.valid_date,day.weather.description))
+        // let city = request.query.cityName;
+        // let lon = request.query.lon;
+        // let lat = request.query.lat;
+        let selectedCity = request.query.cityName
+        let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&city=${selectedCity}&days=3`
+        //console.log(url);
+        let results = await axios.get(url);
+        console.log(results.data.data);
+        // let selectedCity = data.find(x => x.city_name === city);
+        const forecastArray = results.data.data.map(day => new Forecast(day))
+        console.log(forecastArray);
         response.send(forecastArray);
     } catch (error) {
         next(error);
     }
 })
-
+app.get('/movies', async (request, response, next) => {
+    try {
+        let selectedCity = request.query.cityName
+        let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${selectedCity}&page=1&include_adult=false`
+        console.log(url);
+        let results = await axios.get(url);
+       // console.log(results.data.data);
+        // let selectedCity = data.find(x => x.city_name === city);
+        const movieArray = results.data.results.map(data => new MovieArrayClass(data))
+        //console.log(forecastArray);
+        console.log(url);
+        response.send(movieArray);
+    } catch (error) {
+        next(error);
+    }
+})
 
 app.get('*', (request, response) => {
     response.send('That route does not exist');
@@ -42,9 +65,15 @@ app.use((error, request, response, next) => {
 });
 
 class Forecast {
-    constructor (date,description){
-        this.date = date;
-        this.description = description;
+    constructor (data){
+        this.date = data.valid_date;
+        this.description = data.weather.description;
+    }
+}
+class MovieArrayClass {
+    constructor (data){
+        this.title = data.original_title;
+        this.poster = data.poster_path;
     }
 }
 
