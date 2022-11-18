49d0d9a7860adbd87c582eaea2d27452a3529f8e
Oliver Speir <oliverspeir9@gmail.com>
2022-11-18 03:56:09 +0000

    lab09

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 const getWeather = require('./weather.js');
 const getMovies = require ('./movies.js');
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.get('/weather',getWeather);
