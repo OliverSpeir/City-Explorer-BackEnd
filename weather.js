47dd476e2e02622244ee311d7c0f32154779b03d
Oliver Speir <oliverspeir9@gmail.com>
2022-11-18 02:18:09 +0000

    lab09

---
+++ weather.js
@@ -0,0 +1,27 @@
+'use strtict';
+const axios = require('axios');
+require('dotenv').config();
+
+
+
+function getWeather(request, response) {
+  let selectedCity = request.query.cityName
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&city=${selectedCity}&days=3`
+  axios
+    .get(url)
+    .then(results => {
+      const forecastArray = results.data.data.map(day => new Forecast(day));
+      response.status(200).send(forecastArray);
+    })
+    .catch(error => {
+      response.status(500).send(`server error ${error}`);
+    });
+}
+
+class Forecast {
+  constructor(data) {
+    this.date = data.valid_date;
+    this.description = data.weather.description;
+  }
+}
+module.exports = getWeather;
